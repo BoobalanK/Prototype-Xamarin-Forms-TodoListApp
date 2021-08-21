163389e478b20d680fb60a7b84319f49982e0e52
Boobalan Kothandaraman <kboobalan93@gmail.com>
2021-08-21 20:04:49 +0530

    Add project files.

---
+++ TodoListApp/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using FreshMvvm;
+
+using System;
+using System.ComponentModel;
+
+using TodoListApp.PageModels;
+using TodoListApp.Pages;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TodoListApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            InitContainer();
+            var page = FreshPageModelResolver.ResolvePageModel<TodoListPageModel>();
+            var basicNavContainer = new FreshNavigationContainer(page);
+            MainPage = basicNavContainer;
+        }
+
+        private void InitContainer()
+        {
+            //
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
