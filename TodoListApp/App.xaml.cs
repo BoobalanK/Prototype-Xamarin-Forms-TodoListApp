f246b481c6292f2548b7084e5e69cbe4ed4bd6e6
Boobalan Kothandaraman <kboobalan93@gmail.com>
2021-09-05 19:47:22 +0530

    Update App.xaml.cs
    Init Material before InitializeComponeny

--- TodoListApp/App.xaml.cs
+++ TodoListApp/App.xaml.cs
@@ -18,8 +18,8 @@ namespace TodoListApp
     {
         public App()
         {
-            InitializeComponent();
             XF.Material.Forms.Material.Init(this);
+            InitializeComponent();            
             InitContainer();
             var page = FreshPageModelResolver.ResolvePageModel<TodoListPageModel>();
             var basicNavContainer = new FreshNavigationContainer(page);
